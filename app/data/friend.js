691c66a0d4eae17b021b6b1c93ff01a609dec9aa
unknown <ming29398@gmail.com>
2018-11-10 09:13:35 -0800

    updated images

--- app/data/friend.js
+++ app/data/friend.js
@@ -2,8 +2,8 @@
 
 var friendArray = [
     {
-      name: "Ross Geller",
-      photo: "http://68.media.tumblr.com/6cea09c793de512f267ffb4dc783f466/tumblr_mh3c4hFGPu1r9zaago1_500.gif",
+      name: "Andy",
+      photo: "../images/andy.png",
       scores: [
         "5",
         "1",
@@ -18,8 +18,8 @@ var friendArray = [
       ]
     },
     {
-      name: "Phoebe Buffay",
-      photo: "http://25.media.tumblr.com/23e479224f5179ac42665e7a648cf309/tumblr_mndv0cYih81r9zaago1_500.gif",
+      name: "Buzz Lightyear",
+      photo: "../images/buzz-lightyear.jpeg",
       scores: [
         "4",
         "2",
@@ -34,8 +34,8 @@ var friendArray = [
       ]
     },
     {
-      name: "Rachel Green",
-      photo: "http://38.media.tumblr.com/20ad32ddb6e4a8d04dea653b0373dfa6/tumblr_mfv54rXt5l1r9zaago1_500.gif",
+      name: "Hamm",
+      photo: "../images/hamm.png",
       scores: [
         "5",
         "2",
@@ -50,8 +50,8 @@ var friendArray = [
       ]
     },
     {
-      name: "Monica Geller",
-      photo: "https://media.giphy.com/media/beYVvBUHy1Gla/giphy.gif",
+      name: "Lenny",
+      photo: "../images/lenny.jpeg",
       scores: [
         "3",
         "3",
@@ -66,8 +66,8 @@ var friendArray = [
       ]
     },
     {
-      name: "Joey Tribiani",
-      photo: "https://media.giphy.com/media/1FDzUi3TRm4yQ/giphy.gif",
+      name: "Mr. Potato Head",
+      photo: "../images/mr-potato-head.png",
       scores: [
         "4",
         "3",
@@ -82,8 +82,8 @@ var friendArray = [
       ]
     },
     {
-      name: "Chandler Bing",
-      photo: "http://33.media.tumblr.com/cf9686363fde6ca698a9b5b603eef01a/tumblr_ms0bxt7ThZ1r9zaago1_500.gif",
+      name: "Mrs. Potato Head",
+      photo: "../images/mrs-potato-head.png",
       scores: [
         "4",
         "4",
@@ -96,8 +96,71 @@ var friendArray = [
         "4",
         "5"
       ]
-    }
-  
+    },
+    {
+      name: "Rex",
+      photo: "../images/Rex.png",
+      scores: [
+        "2",
+        "3",
+        "1",
+        "3",
+        "5",
+        "2",
+        "3",
+        "1",
+        "4",
+        "5"
+      ]
+    },
+    {
+      name: "Slinky Dog",
+      photo: "../images/slinky-dog.png",
+      scores: [
+        "2",
+        "3",
+        "1",
+        "3",
+        "5",
+        "2",
+        "3",
+        "1",
+        "4",
+        "5"
+      ]
+    },
+    {
+      name: "Stretch",
+      photo: "../images/stretch.png",
+      scores: [
+        "1",
+        "3",
+        "1",
+        "2",
+        "3",
+        "2",
+        "3",
+        "4",
+        "4",
+        "5"
+      ]
+    },
+    {
+      name: "Woody",
+      photo: "../images/woody.png",
+      scores: [
+        "5",
+        "5",
+        "4",
+        "4",
+        "3",
+        "3",
+        "2",
+        "2",
+        "1",
+        "1"
+      ]
+    } 
   ];
   
   // export the array
